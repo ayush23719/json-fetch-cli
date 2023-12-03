fc427673398a8375db5e3172fdcf2961505008e4
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-12-03 19:44:40 +0530

    -retrievePosts

---
+++ index.js
@@ -0,0 +1,20 @@
+import program from "commander";
+
+program.description("Access the JSON Placeholder API");
+program.name("jsonf");
+program.usage("<command>");
+program.addHelpCommand(false);
+program.helpOption(false);
+
+program
+    .command("posts")
+    .argument("[postId]", "ID of post you'd like to retrieve.")
+    .option("-p, --pretty", "Pretty-print output from the API.")
+    .description(
+        "Retrieve a list of all posts or one post by passing the post ID (e.g., posts 1)."
+    )
+    .action(posts);
+
+program.parse(process.argv);
+
+program.parse(process.argv);
