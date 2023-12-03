ca5b07c60d9109efc7b4938a0e4ac33f4cf284d7
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-12-03 20:04:23 +0530

    -commentsAndUsers

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { program } from "commander";
+import posts from "./commands/posts.js";
+import comments from "./commands/comments.js";
+import users from "./commands/users.js";
 
 program.description("Access the JSON Placeholder API");
 program.name("jsonf");
@@ -14,5 +17,20 @@ program
         "Retrieve a list of all posts or one post by passing the post ID (e.g., posts 1)."
     )
     .action(posts);
+
+program
+    .command("comments")
+    .option("-p, --pretty", "Pretty-print output from the API.")
+    .description("Retrieve a list of all comments.")
+    .action(comments);
+
+program
+    .command("users")
+    .argument("[userId]", "ID of the user you'd like to retrieve.")
+    .option("-p, --pretty", "Pretty-print output from the API.")
+    .description(
+        "Retrieve a list of all users or one user by passing the user ID (e.g., users 1)."
+    )
+    .action(users);
 
 program.parse(process.argv);
